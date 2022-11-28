d20286319b539014fe477f0e680c6c2459d787aa
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-11-28 23:49:55 +0100

    add date picker

--- src/lib/components/Application/Application.svelte
+++ src/lib/components/Application/Application.svelte
@@ -2,13 +2,22 @@
 	import type { Application } from '$lib/application';
 	import Interviews from './Interviews.svelte';
 	export let data: Application;
+
+	let date = new Date().toLocaleDateString();
+
+	let handleApplied = () => {
+		// todo handle data
+	};
 </script>
 
 <article>
-	<h3>{data.companyName}</h3>
-	<p>{data.jobTitle}</p>
+	<h3>{data.companyName} - {data.jobTitle}</h3>
 	{#if data.appliedDate}
 		<p>applied on: {data.appliedDate.toDateString()}</p>
+	{:else}
+		<p>not yet applied</p>
+		<input type="date" name="applyDate" id="applyDate" value={date} />
+		<button on:click={handleApplied}>applied</button>
 	{/if}
 
 	<div>
