c7587cf3f07030226eca13ed76b8d33de562a5f2
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-12-05 20:07:42 +0100

    add user profile component

---
+++ src/lib/components/Nav/Profile.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import profile from '$lib/assets/user-circle-svgrepo-com.svg';
+	export let name: string | null | undefined;
+	export let profileUrl: string | null | undefined;
+</script>
+
+<div class="profile">
+	<a href="/dashboard/account">
+		<h2 class="title-large">{name}</h2>
+		{#if profileUrl}
+			<img src={profileUrl} alt="profile" />
+		{/if}
+		{#if !profileUrl}
+			<img src={profile} alt="profile" />
+		{/if}
+	</a>
+</div>
+
+<style>
+	.profile {
+		border-radius: var(--my-border-radius);
+	}
+
+	.profile:hover {
+		background: var(--md-sys-color-surface-1);
+	}
+
+	a {
+		display: block;
+		padding: 2rem 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 2rem;
+
+		text-decoration: none;
+		color: rgb(var(--md-sys-color-on-background));
+	}
+
+	h2 {
+		margin: 0;
+		text-align: center;
+	}
+
+	img {
+		width: 6rem;
+		height: 6rem;
+		object-fit: fill;
+		border-radius: 50%;
+	}
+</style>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -3,6 +3,7 @@
 	import { user } from '../../lib/stores/auth';
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/environment';
+	import Profile from '$lib/components/Nav/Profile.svelte';
 
 	onMount(() => {
 		if (!$user) {
@@ -15,18 +16,43 @@
 			goto('/login');
 		}
 	}
+
+	$: profileAgs = {
+		name: $user?.displayName,
+		profileUrl: $user?.photoURL
+	};
 </script>
 
-<aside>
-	<nav>
-		<ul>
-			<li><a href="/dashboard/applications">applications</a></li>
-			<li><a href="/dashboard/interim">interim</a></li>
-			<li><a href="/dashboard/account">account</a></li>
-			<li><a href="/dashboard/settings">settings</a></li>
-		</ul>
-	</nav>
-</aside>
-<main>
-	<slot />
-</main>
+<div class="page">
+	<aside>
+		<nav>
+			<ul>
+				<li><Profile {...profileAgs} /></li>
+				<li><a href="/dashboard/applications">applications</a></li>
+				<li><a href="/dashboard/interim">interim</a></li>
+				<li><a href="/dashboard/settings">settings</a></li>
+			</ul>
+		</nav>
+	</aside>
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.page {
+		display: flex;
+	}
+	aside {
+		width: 20rem;
+		min-height: 100vh;
+		padding: 1rem 1.2rem;
+	}
+	ul {
+		padding: 0;
+		margin: 0;
+	}
+	li {
+		list-style: none;
+	}
+</style>
