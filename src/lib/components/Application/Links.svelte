0d88a6128ed15cc732bfb86be3fc845a3a2fe08b
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-12-10 23:52:39 +0100

    add forms to add page for link and event

--- src/lib/components/Application/Links.svelte
+++ src/lib/components/Application/Links.svelte
@@ -1,23 +1,10 @@
 <script lang="ts">
-	import { createLink, type Link } from '$lib/Link';
+	import type { Link } from '$lib/Link';
 
 	export let links: Link[];
-
-	let link = createLink('');
-	let handleAddLink = () => {
-		links = [link, ...links];
-		link = createLink('');
-	};
 </script>
 
 <div>
-	<form action="" on:submit|preventDefault={handleAddLink}>
-		<label for="">name</label>
-		<input type="text" bind:value={link.name} />
-		<label for="">url</label>
-		<input type="url" bind:value={link.url} />
-		<button type="submit">add link</button>
-	</form>
 	<ul>
 		{#each links as link}
 			<li>
--- src/routes/(app)/dashboard/applications/add/+page.svelte
+++ src/routes/(app)/dashboard/applications/add/+page.svelte
@@ -2,9 +2,15 @@
 	import { applicationToFirestore, createApplication } from '$lib/Application';
 	import { createEvent } from '$lib/Event';
 	import { user } from '$lib/stores/auth';
+	import type { IEvent } from '$lib/Event';
+	import type { Link } from '$lib/Link';
+	import Links from '$lib/components/Application/Links.svelte';
 
 	let companyName = '';
 	let jobTitle = '';
+
+	let links: Link[] = [];
+	let events: IEvent[] = [];
 
 	const handleAddApplication = () => {
 		if (!$user?.uid) return;
@@ -13,12 +19,63 @@
 		application.events.push(creationEvent);
 		applicationToFirestore($user.uid, application);
 	};
+
+	const handleAddLink = () => {
+		//TODO: implement
+	};
+
+	const handleAddEvent = () => {
+		//TODO: implement
+	};
 </script>
 
-<form on:submit|preventDefault={handleAddApplication}>
-	<label for="job-title">job title</label>
-	<input type="text" name="job-title" id="job-title" bind:value={jobTitle} required />
-	<label for="company-name">company</label>
-	<input type="text" name="company-name" id="company-name" bind:value={companyName} required />
-	<button type="submit">add application</button>
-</form>
+<h2>add application</h2>
+<section>
+	<h3>application info</h3>
+	<form on:submit|preventDefault={handleAddApplication}>
+		<label for="job-title">job title</label>
+		<input type="text" name="job-title" id="job-title" bind:value={jobTitle} required />
+		<label for="company-name">company</label>
+		<input type="text" name="company-name" id="company-name" bind:value={companyName} required />
+	</form>
+</section>
+<section>
+	<h3>links</h3>
+	<Links {links} />
+	<form on:submit|preventDefault={handleAddLink}>
+		<label for="name">name</label>
+		<input type="text" name="name" id="link-name" />
+		<label for="url">link</label>
+		<input type="url" name="url" id="link-url" />
+		<button type="submit">add link</button>
+	</form>
+</section>
+<section>
+	<h3>events</h3>
+	<p>TODO: create events component</p>
+
+	<form on:submit|preventDefault={handleAddEvent}>
+		<label for="title">title</label>
+		<input type="text" name="title" id="event-title" />
+		<label for="description">description</label>
+		<input type="text" name="description" id="event-description" />
+		<label for="start-time">start time</label>
+		<input type="datetime-local" name="start-time" id="event-start-time" />
+		<label for="end-time">end time</label>
+		<input type="datetime-local" name="end-time" id="event-end-time" />
+		<label for="location">location</label>
+		<input type="text" name="location" id="event-location" />
+		<button type="submit">add event</button>
+	</form>
+</section>
+<button type="submit" on:click|preventDefault={handleAddApplication}>add application</button>
+
+<style>
+	label {
+		display: block;
+	}
+
+	button {
+		display: block;
+	}
+</style>
