71f0a9a6c979db1b3545b670c1926bc20a2aef16
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2023-01-05 23:24:36 +0100

    created new ApplicationForm component

---
+++ src/routes/(app)/dashboard/applications/(ops)/ApplicationForm.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Links from './Links.svelte';
+	import Events from './Events.svelte';
+	import AddLinkForm from './AddLinkForm.svelte';
+	import AddEventForm from './AddEventForm.svelte';
+	import type { Application } from '$lib/Application';
+
+	export let application: Application;
+</script>
+
+<section>
+	<h3>application info</h3>
+	<form on:submit|preventDefault>
+		<label for="job-title">job title</label>
+		<input type="text" name="job-title" id="job-title" bind:value={application.jobTitle} required />
+		<label for="company-name">company</label>
+		<input
+			type="text"
+			name="company-name"
+			id="company-name"
+			bind:value={application.companyName}
+			required
+		/>
+	</form>
+</section>
+<section>
+	<h3>links</h3>
+	<Links links={application.links} />
+	<AddLinkForm bind:links={application.links} />
+</section>
+<section>
+	<h3>events</h3>
+	<Events events={application.events} />
+	<AddEventForm bind:events={application.events} />
+</section>
+
+<style>
+	label {
+		display: block;
+	}
+</style>
