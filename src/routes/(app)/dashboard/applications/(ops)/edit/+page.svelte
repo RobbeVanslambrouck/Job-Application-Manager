014b8e82b9167eca7871e0ada5a41ecbe543a1a2
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2023-01-12 00:30:23 +0100

    remove delete action in edit page

--- src/routes/(app)/dashboard/applications/(ops)/edit/+page.svelte
+++ src/routes/(app)/dashboard/applications/(ops)/edit/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { applicationToFirestore, deleteApplicationFromFirestore } from '$lib/Application';
+	import { applicationToFirestore } from '$lib/Application';
 	import { user } from '$lib/stores/auth';
 	import type { PageData } from './$types';
 	import { goto } from '$app/navigation';
@@ -8,48 +8,21 @@
 	export let data: PageData;
 	let application = data.application;
 
-	let isDeletable = false;
 	let isExecutingAction = false;
 
-	const handleAction = async (action: 'DELETE' | 'EDIT') => {
+	const handleEdit = async () => {
 		if (!$user?.uid) return;
 		isExecutingAction = true;
-		if (action === 'DELETE') await deleteApplicationFromFirestore($user.uid, application.id);
-		if (action === 'EDIT') await applicationToFirestore($user.uid, application);
+		await applicationToFirestore($user.uid, application);
 		goto('/dashboard/applications/');
 	};
 </script>
 
 <h2 class="sr-only">edit application</h2>
-<ApplicationForm
-	bind:application
-	on:done={() => {
-		handleAction('EDIT');
-	}}
-/>
-{#if !isExecutingAction}
-	<form on:submit|preventDefault>
-		<label for="delete">i want to delete this application</label>
-		<input type="checkbox" name="delete-application" id="delete" bind:checked={isDeletable} />
-		<button
-			type="button"
-			on:click={() => {
-				handleAction('DELETE');
-			}}
-			disabled={!isDeletable}>delete</button
-		>
-	</form>
-{/if}
+<ApplicationForm bind:application on:done={handleEdit} />
 {#if isExecutingAction}
 	<iconify-icon icon="line-md:loading-twotone-loop" />
 {/if}
 
 <style>
-	label {
-		display: block;
-	}
-
-	button {
-		display: block;
-	}
 </style>
