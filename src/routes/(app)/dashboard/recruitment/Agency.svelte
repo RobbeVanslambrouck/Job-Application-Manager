0235f79fb7cad48caaa624770fc886422de952f2
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2023-01-22 23:53:16 +0100

    added agency view page

--- src/routes/(app)/dashboard/recruitment/Agency.svelte
+++ src/routes/(app)/dashboard/recruitment/Agency.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import type { RecruitmentAgency } from '$lib/recruitmentAgency';
 
 	export let agency: RecruitmentAgency;
+
+	let { name, contacts, proposedCompanies } = agency;
+
+	function handleDblClick() {
+		goto(`/dashboard/recruitment/agency?name=${name}`);
+	}
 </script>
 
-<p>{agency.name}</p>
+<article tabindex="-1" on:dblclick={handleDblClick}>
+	<h3>{name}</h3>
+</article>
