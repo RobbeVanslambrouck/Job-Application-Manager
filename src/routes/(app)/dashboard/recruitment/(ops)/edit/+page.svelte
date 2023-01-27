de0379fedfd9d2048d2900d0ed19d5b1fd391be0
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2023-01-27 21:24:19 +0100

    fix agency edit

--- src/routes/(app)/dashboard/recruitment/(ops)/edit/+page.svelte
+++ src/routes/(app)/dashboard/recruitment/(ops)/edit/+page.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { user } from '$lib/stores/auth';
-	import { agencyToFirestore } from '$lib/recruitmentAgency';
+	import { agencyToFirestore, deleteAgencyFromFirestore } from '$lib/recruitmentAgency';
 	import { goto } from '$app/navigation';
 	import AgencyForm from '../AgencyForm.svelte';
 
 	export let data: PageData;
 	let agency = data.agency;
+	let oldName = agency.name;
 
 	let isExecutingAction = false;
 
 	const handleEdit = async () => {
 		if (!$user?.uid) return;
 		isExecutingAction = true;
+		if (oldName !== agency.name) {
+			await deleteAgencyFromFirestore($user.uid, oldName);
+		}
 		await agencyToFirestore($user.uid, agency);
 		goto('/dashboard/recruitment/');
 	};
